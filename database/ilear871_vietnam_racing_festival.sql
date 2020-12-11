4e327c2b90876f672a4e9c6877bfe2c475abb2b7
Thai <trind06@gmail.com>
2020-12-11 14:23:52 +0700

    database action

--- database/ilear871_vietnam_racing_festival.sql
+++ database/ilear871_vietnam_racing_festival.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2020 at 03:06 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.31
+-- Generation Time: Dec 11, 2020 at 08:20 AM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,6 +25,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `auction`
+--
+
+CREATE TABLE `auction` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(300) CHARACTER SET utf8 DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `phone` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `address` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `position_level` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `pid_rate` int(11) DEFAULT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `auction`
+--
+
+INSERT INTO `auction` (`id`, `fullname`, `birthday`, `phone`, `email`, `address`, `position_level`, `pid_rate`, `create_date`) VALUES
+(1, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:20:38'),
+(2, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:21:02'),
+(3, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:25:06'),
+(4, 'thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:25:33'),
+(5, 'thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:26:14'),
+(6, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:26:49'),
+(7, 'thai', '1994-10-10', 'Hoa Kỳ tại Pebble Beach Golf Links lịch ', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:31:28'),
+(8, 'thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:52:51'),
+(9, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 09:56:11'),
+(10, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 11:07:39'),
+(11, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 11:13:21'),
+(12, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 11111, '2020-12-11 13:16:55'),
+(13, 'thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai@gmail.com', 'ssss', 'ssd', 11111, '2020-12-11 13:22:48'),
+(14, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 333333, '2020-12-11 13:28:54'),
+(15, 'ssss1111', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 11111, '2020-12-11 13:35:43'),
+(16, 'ssss1111', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 11111, '2020-12-11 13:37:52'),
+(17, 'thai1', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1111, '2020-12-11 13:38:27'),
+(18, 'aaaa1', '1994-10-10', '+84.987959209', 'vohuyenthienthai@gmail.com', 'ssss', 'ssd', 1111, '2020-12-11 13:39:09'),
+(19, 'aaaa2', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 11111, '2020-12-11 13:42:29'),
+(20, 'aaaaqqq', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 13:51:53'),
+(21, 'aaaaqqq', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 13:59:00'),
+(22, 'dung', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 13:59:26'),
+(23, 'dung', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 13:59:49'),
+(24, 'dung', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 14:00:25'),
+(25, 'dung', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 14:04:11'),
+(26, 'dung', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 14:06:35'),
+(27, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1111, '2020-12-11 14:07:17'),
+(28, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1233, '2020-12-11 14:08:06'),
+(29, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1233, '2020-12-11 14:08:13'),
+(30, 'aaaa', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1233, '2020-12-11 14:10:13'),
+(31, 'thien thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 14:15:01'),
+(32, 'thien thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai16@gmail.com', 'ssss', 'ssd', 1200, '2020-12-11 14:16:03'),
+(33, 'thien thai', '1994-10-10', '+84.987959209', 'vohuyenthienthai94@gmail.com', 'ssss', 'ssd', 1111, '2020-12-11 14:17:01');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `caulacbo`
 --
 
@@ -33,7 +91,7 @@ CREATE TABLE `caulacbo` (
   `title` varchar(300) DEFAULT NULL,
   `description` varchar(1000) DEFAULT NULL,
   `hyperlink` varchar(300) DEFAULT NULL,
-  `create_date` datetime NOT NULL DEFAULT current_timestamp()
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,6 +100,27 @@ CREATE TABLE `caulacbo` (
 
 INSERT INTO `caulacbo` (`id`, `image_name`, `title`, `description`, `hyperlink`, `create_date`) VALUES
 (1, 'assets/clbs/Untitled_page-0041.jpg', 'HỘI GOLF TỈNH ĐỒNG NAI', NULL, NULL, '2020-12-09 22:04:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plan`
+--
+
+CREATE TABLE `plan` (
+  `id` int(11) NOT NULL,
+  `title_plan` varchar(300) DEFAULT NULL,
+  `plan_description` varchar(1000) DEFAULT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `plan`
+--
+
+INSERT INTO `plan` (`id`, `title_plan`, `plan_description`, `create_date`) VALUES
+(4, 'video', 'sukien', '2020-12-10 10:23:16'),
+(5, 'video1', 'sukien1', '2020-12-10 10:23:16');
 
 -- --------------------------------------------------------
 
@@ -66,7 +145,7 @@ CREATE TABLE `registers` (
   `comment1` varchar(1000) DEFAULT NULL,
   `comment2` varchar(1000) DEFAULT NULL,
   `comment3` varchar(1000) DEFAULT NULL,
-  `create_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `form_id` varchar(100) DEFAULT NULL,
   `form_name` varchar(400) DEFAULT NULL,
   `extra_column1` varchar(300) DEFAULT NULL,
@@ -102,7 +181,7 @@ CREATE TABLE `users` (
   `address` varchar(300) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `country` varchar(100) DEFAULT NULL,
-  `create_date` datetime NOT NULL DEFAULT current_timestamp()
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -117,9 +196,21 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `address`, `birthday
 --
 
 --
+-- Indexes for table `auction`
+--
+ALTER TABLE `auction`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- Indexes for table `caulacbo`
 --
 ALTER TABLE `caulacbo`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `plan`
+--
+ALTER TABLE `plan`
   ADD UNIQUE KEY `id` (`id`);
 
 --
@@ -140,10 +231,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `auction`
+--
+ALTER TABLE `auction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- AUTO_INCREMENT for table `caulacbo`
 --
 ALTER TABLE `caulacbo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `plan`
+--
+ALTER TABLE `plan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `registers`
