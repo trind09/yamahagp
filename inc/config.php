6340cc6237689ba47494c90ec44ef029b353bce2
Thai <trind06@gmail.com>
2020-12-26 08:41:05 +0700

    config

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,7 @@ $domain = "http://localhost/yamahagp/";
 $root = "E:/xampp7/htdocs/yamahagp/";
 $dbhost = 'localhost';
 // Database Name
-$dbname = 'ilear871_vietnam_racing_festival';
+$dbname = 'daotaome_golfandcar';
 // Database Username
 $dbuser = 'root';
 // Database Password
